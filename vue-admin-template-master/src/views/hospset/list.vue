cfd562e8946ba00654ad65c7eb35f6405372ee69
YUXINLU <yuxin.lu@thoughtworks.com>
2022-01-04 11:45:17 +0800

    医院设置-查询列表（分页加条件）

--- vue-admin-template-master/src/views/hospset/list.vue
+++ vue-admin-template-master/src/views/hospset/list.vue
@@ -1,6 +1,43 @@
 <template>
   <div class="app-container">
       医院设置列表
+    <el-form :inline="true" class="demo-form-inline">
+        <el-form-item>
+            <el-input  v-model="searchObj.hosname" placeholder="医院名称"/>
+        </el-form-item>
+        <el-form-item>
+            <el-input v-model="searchObj.hoscode" placeholder="医院编号"/>
+        </el-form-item>
+        <el-button type="primary" icon="el-icon-search" @click="getList()">查询</el-button>
+    </el-form>
+
+
+      <!-- banner列表 -->
+        <el-table
+        :data="list" stripe style="width: 100%">
+            <el-table-column type="index" width="50" label="序号"/>
+            <el-table-column prop="hosname" label="医院名称"/>
+            <el-table-column prop="hoscode" label="医院编号"/>
+            <el-table-column prop="apiUrl" label="api基础路径" width="200"/>
+            <el-table-column prop="contactsName" label="联系人姓名"/>
+            <el-table-column prop="contactsPhone" label="联系人手机"/>
+            <el-table-column label="状态" width="80">
+                <template slot-scope="scope">
+                        {{ scope.row.status === 1 ? '可用' : '不可用' }}
+                </template>
+            </el-table-column>
+        </el-table>
+
+        <!-- 分页 -->
+        <el-pagination
+        :current-page="current"
+        :page-size="limit"
+        :total="total"
+        style="padding: 30px 0; text-align: center;"
+        layout="total, prev, pager, next, jumper"
+        @current-change="getList"/>
+
+
   </div>
 </template>
 
@@ -22,7 +59,8 @@ export default {
         this.getList()
     },
     methods: {
-        getList() {
+        getList(page=1) {
+            this.current = page
             hospset.getHospSetList(this.current,this.limit,this.searchObj)
                 .then(response => {
                     this.list = response.data.records
